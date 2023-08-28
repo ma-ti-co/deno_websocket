6aa6ebcad90cf7842902028f43109e0a5f38a7cf
Matthias <campe@berlinale.de>
2023-08-28 13:45:49 +0200

    fixed_websocket

--- index.ts
+++ index.ts
@@ -5,9 +5,12 @@ const connectedClients = new Map();
 const wss = new WebSocketServer(8080);
 wss.on("connection", function (ws: WebSocketClient) {
   //broadcast_usernames();
-  ws.on("message", function (message: string) {
-    console.log(message);
-    ws.send(message);
+
+  ws.on("message", function (response: string) {
+    let payload = JSON.parse(response);
+    let message = JSON.parse(payload.message);
+    connectedClients.set(message.user, ws)
+    broadcast_usernames()
   });
 });
 
@@ -16,24 +19,26 @@ wss.on('close', function (ws: WebSocketClient) {
 })
 
 // // send a message to all connected clients
-// function broadcast(message) {
-//   for (const client of connectedClients.values()) {
-//     client.send(message);
-//   }
-// }
+function broadcast(message:any) {
+
+  for (const client of connectedClients.values()) {
+    console.log(message);
+    client.send(message);
+  }
+}
 
 // // send updated users list to all connected clients
-// function broadcast_usernames() {
-//   const user_ids = [...connectedClients.keys()];
-//   console.log(
-//     "Sending updated username list to all clients: " +
-//       JSON.stringify(user_ids),
-//   );
-//   broadcast(
-//     JSON.stringify({
-//       event: "update-users",
-//       user_ids: user_ids,
-//     }),
-//   );
-// }
+function broadcast_usernames() {
+  const user_ids = [...connectedClients.keys()];
+  console.log(
+    "Sending updated username list to all clients: " +
+      JSON.stringify(user_ids),
+  );
+  broadcast(
+    JSON.stringify({
+      event: "update_users",
+      user_ids: user_ids,
+    }),
+  );
+}
 
